e3f68b126f826c5aa9c62552e82081e673184ace
Sankar147 <136622137+Sankar147@users.noreply.github.com>
2023-07-12 22:44:55 +0530

    webhook chenges

--- consumerBanking/src/main/java/com/companyname/bank/App.java
+++ consumerBanking/src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "webhook chenges on github" );
+        System.out.println( "webhook chenges" );
     }
 }
